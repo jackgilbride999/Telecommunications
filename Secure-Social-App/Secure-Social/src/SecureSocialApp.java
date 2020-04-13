f37a316e32f1c0203c6daa65f1f02dbefe2ed471
Jack Gilbride <jackgilbride999@gmail.com>
2020-04-13 15:08:46 +0100

    Moved to Eclipse project to handle dependencies on MongoDB. IDEs are great

--- Secure-Social-App/Secure-Social/src/SecureSocialApp.java
+++ Secure-Social-App/Secure-Social/src/SecureSocialApp.java
@@ -2,19 +2,38 @@ import java.lang.*;
 import java.io.*;
 import java.util.*;
 
+import org.bson.Document;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClientSettings;
+
+
 public class SecureSocialApp {
     public static void main(String[] args) {
         String[] passwords = getPasswords();
         String readOnlyPassword = passwords[0];
         String readWritePassword = passwords[1];
+
+        MongoClient mongoClient = MongoClients.create(
+                "mongodb+srv://readwrite:" + readWritePassword +"@secure-social-0n8uh.azure.mongodb.net/test?retryWrites=true&w=majority");
+        MongoDatabase database = mongoClient.getDatabase("Secure-Social");
+        System.out.println(database.getName());
+        for(Document doc : database.listCollections()) {
+        	System.out.println(doc.toJson());
+        }
+
+
     }
 
+    
     /*
-        Return the passwords to the data base in a String array.
-        index 0 contains the read-only password
-        index 1 contains the read-write password
-    */
-    private static String[] getPasswords(){
+     * Return the passwords to the data base in a String array. index 0 contains the
+     * read-only password index 1 contains the read-write password
+     */
+    private static String[] getPasswords() {
         try {
             String[] passwords = new String[2];
 
