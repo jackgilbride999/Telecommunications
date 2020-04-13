3c1fbf070682e1c2f3727d24b40c44e509a62389
Jack Gilbride <jackgilbride999@gmail.com>
2020-04-13 13:32:59 +0100

    Some file reading

--- Secure-Social-App/SecureSocialApp.java
+++ Secure-Social-App/SecureSocialApp.java
@@ -1,4 +1,17 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
 public class SecureSocialApp {
     public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("readonlypassword.txt");
+            Scanner fileScanner = new Scanner(fileReader);
+            String readOnlyPassword = fileScanner.nextLine();
+            System.out.println(readOnlyPassword);
+            fileScanner.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
